b85ee0b52ebfe4c4377b667f8282c8b374f9f977
Chris Clark <chrisclark@nhs.net>
2019-07-04 16:00:08 +0000

    a second minor change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-# My file
+# My newer file
 openssl s_client -connect ${DTR_IP}:443 -showcerts \
     </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee \
     /usr/local/share/ca-certificates/${DTR_IP}.crt
 sudo update-ca-certificates
-
+cat entrypoint.sh > test.txt
 /sbin/tini -- /usr/local/bin/jenkins.sh
